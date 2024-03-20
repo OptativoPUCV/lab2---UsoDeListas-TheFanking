be4e261de3f4bbeee324f2da7ec8215e6e71c6e1
Your Name <you@example.com>
2024-03-20 21:56:39 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -46,10 +46,7 @@ List* crea_lista() {
    int numero[10] = {1,2,3,4,5,6,7,8,9,10};
    for (int i = 0 ; i < 10  ; i++)
      {
-       L = realloc(L,sizeof(10));
        pushFront(L,&numero[i]);
-         
-       
      }
    return L;
 }
