848cc06a9672b8718c485dbfe45c139fb46b1ba0
Your Name <you@example.com>
2024-03-23 21:20:30 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -147,11 +147,11 @@ int parentesisBalanceados(char *cadena)
       }
       else
       {
-        if (top(pila) == NULL) return 1;
+        if (top(pila) == NULL) return 0;
       }
       pop(pila);
     }
-  if (top(pila) == NULL) return 1;
+  if (top(pila) == NULL) return 0;
   return 1;
 }
 
